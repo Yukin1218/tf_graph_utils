7a508b96e09513ee64556a881e287970f7da7eae
zhihui <fuzhihui@zhongan.io>
2019-04-08 16:06:03 +0800

    freeze

--- freeze.py
+++ freeze.py
@@ -2,6 +2,13 @@
 import tensorflow as tf
 
 def freeze_model(cp_path, output_node_names, output_file='frozen.pb'):
+    """freeze tf models
+
+    Args:
+        cp_path: tf checkpoint path with checkpoint file
+        output_node_names: model output nodes
+        output_file: frozen graph protobuf serialize file
+    """
     sess = tf.Session()
     ckp_state = tf.train.get_checkpoint_state(cp_path)
     saver = tf.train.import_meta_graph(
@@ -10,7 +17,7 @@ def freeze_model(cp_path, output_node_names, output_file='frozen.pb'):
     saver.restore(sess, ckp_state.model_checkpoint_path)
 
     graph = tf.get_default_graph()
-    input_graph_def = graph.as_graph_def()
+    input_graph_def = graph.as_graph_def(add_shapes=True)
 
     output_graph_def = tf.graph_util.convert_variables_to_constants(
         sess,
